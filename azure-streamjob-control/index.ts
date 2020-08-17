45fe012649907356786682e91fd243a2ce10c574
Steven Hoang <steven.hoang@transwap.com>
2020-08-17 16:48:05 +0800

    update

--- azure-streamjob-control/index.ts
+++ azure-streamjob-control/index.ts
@@ -10,9 +10,6 @@ async function run() {
     const sv = task.getInput('azureSubscriptionEndpoint', true);
     const subId = task.getEndpointDataParameter(sv, 'subscriptionId', false);
 
-    // console.log('AzServiceId', sv);
-    // console.log('SubId', subId);
-
     const group = task.getInput('resourceGroupName', true);
     const streamJobName = task.getInput('azStreamJobName', true);
     const action = task.getInput('action', true).toLowerCase();
@@ -29,23 +26,27 @@ async function run() {
     console.log('endpoint', url);
 
     const token = await auth.getToken();
-    axios.post(
-      url,
-      {},
-      { headers: { Authorization: 'Bearer ' + token.accessToken } },
-    ).then(res => {
-      console.log(res);
-    })
-      .catch(error => {
-        if (error.response && ERROR_STATUS_CODES.includes(error.response.status)) {
+    axios
+      .post(
+        url,
+        {},
+        { headers: { Authorization: 'Bearer ' + token.accessToken } },
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        if (
+          error.response &&
+          ERROR_STATUS_CODES.includes(error.response.status)
+        ) {
           console.log(error.response.status);
           task.setResult(task.TaskResult.Failed, error.message);
         }
-      })
+      });
 
     task.setResult(task.TaskResult.Succeeded, '', true);
   } catch (err) {
-    
     console.log(`Failed to send req: ${JSON.stringify(err)}`);
     task.setResult(task.TaskResult.Failed, err.message);
   }
